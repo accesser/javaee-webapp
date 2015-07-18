c09cddd7e551e7f3a627288e85deccbd74dd9f05
MT-01 <koiwa.cats@gmail.com>
2015-07-18 20:02:12 +0900

    javaee-webapp

---
+++ src/main/java/sample/HelloWorld.java
@@ -0,0 +1,20 @@
+package sample;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "HelloWorld", urlPatterns = { "/HelloWorld" })
+public class HelloWorld extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse res)
+			throws IOException, ServletException {
+
+		req.getRequestDispatcher("helloworld.jsp").forward(req, res);
+	}
+
+}
